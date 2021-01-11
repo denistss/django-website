e454eb8cc00905d1c48769b587c7d4d1fea0b581
Denis Tiago Silva <56735135+denistss@users.noreply.github.com>
2021-01-11 15:48:40 -0300

    urls-views do menu

--- hackershack_website/settings.py
+++ hackershack_website/settings.py
@@ -14,7 +14,7 @@ from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+PROJECT_DIR = Path.joinpath(BASE_DIR, "hackershack_website") 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'hackershack_website.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [Path.joinpath(PROJECT_DIR, "templates")],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
